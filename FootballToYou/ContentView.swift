8ca0e52319cc5c74ecb29933645143b7200c6bc7
Akhil Eaga <akhil.eaga@bhp.com>
2025-12-29 07:59:29 +0800

    Updates after the challlenge is finished.

--- FootballToYou/ContentView.swift
+++ FootballToYou/ContentView.swift
@@ -10,10 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What is Football to You?")
+                .font(.largeTitle)
+                .fontWeight(.medium)
+            HStack {
+                Image(systemName: "figure.american.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.blue.gradient)
+                Image(systemName: "figure.australian.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.indigo.gradient)
+                Image(systemName: "figure.indoor.soccer")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.purple.gradient)
+            }
         }
         .padding()
     }
