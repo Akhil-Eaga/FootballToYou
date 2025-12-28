c07c576340d2f3f454f8d77c9bba50080d02ac2a
Akhil Eaga <akhil.eaga@bhp.com>
2025-12-28 14:40:47 +0530

    Initial Commit

---
+++ FootballToYou/FootballToYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballToYouApp.swift
+//  FootballToYou
+//
+//  Created by Akhil Eaga on 28/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct FootballToYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
